bdcecb1b68e0253bec0691f33957cb4f85aefb3e
Ahmed Hassan <a.hassan@buismed.de>
2014-09-03 13:58:16 +0200

    updated to meteor 0.9

--- package.js
+++ package.js
@@ -1,16 +1,19 @@
 Package.describe({
-    summary: "Adding a easy to use settings manager"
+	summary: "Adding a easy to use settings manager",
+	version: "0.3.0",
+	git: "https://github.com/nerdmed/settings-manager.git"
 });
 
-Package.on_use(function(api, where) {
-    api.use(['reactive-dict', 'underscore', 'ejson', 'deps', 'ui'], 'client');
-    api.add_files(['lib/settings.js'], 'client');
 
-    if (api.export)
-        api.export('SettingsManager');
+Package.onUse(function(api) {
+	api.versionsFrom('METEOR@0.9.0.1');
+	api.use(['reactive-dict', 'underscore', 'ejson', 'deps', 'ui'], 'client');
+	api.add_files(['lib/settings.js'], 'client');
+	api.export('SettingsManager');
 });
+
 
 Package.on_test(function(api) {
-    api.use(['underscore', 'ejson', 'deps', 'settings-manager', 'spacebars', 'tinytest', 'test-helpers']);
-    api.add_files(['tests/basictests.js'], 'client');
+	api.use(['underscore', 'ejson', 'deps', 'nerdmed:settings-manager', 'spacebars', 'tinytest', 'test-helpers']);
+	api.add_files(['tests/basictests.js'], 'client');
 })
